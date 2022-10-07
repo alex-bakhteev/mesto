26c97786a3c37d7def1d2946eacb5049701e3aa8
Alexander Bakhteev <alex-bakhteev@yandex.ru>
2022-10-08 02:32:24 +0300

    version 3

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,18 @@ let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeIcon = document.querySelector('.popup__close-icon');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__field-name');
-let jobInput = document.querySelector('.popup__field-occupation');
+let nameInput = document.querySelector('.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field_type_occupation');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+/*
 let like = document.querySelectorAll('.elements__like');
 let likeButton = document.querySelector('.elements__like');
+*/
 
 function openPopup() {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -21,14 +25,17 @@ function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+    closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
 closeIcon.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
+/*
 like.forEach((likeButton) => {
     likeButton.addEventListener("click", () => {
         likeButton.classList.toggle("elements__like_active");
     });
 });
+*/
